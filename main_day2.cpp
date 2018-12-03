35dc1dd9770958105ee87eed8f5ee0fadb0e6abb
PeterTillema <petertillema@solcon.nl>
2018-12-03 20:22:18 +0100

    First commi

---
+++ main_day2.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <sstream>
+#include <map>
+
+int main() {
+    std::string data = "wkzhyfdpluzeqvajtbbosngkxc wrzhyfdplumeqvajtbioskfksc wrzhyfdolumyqvajtbiosngkxs urzhyfdplbmeqvrjtbiosngkxc wrzhyfrulumeqvajtbiosngkxf wrzhnfdprumvqvajtbiosngkxc wrzhyfpplumeivajtbiosrgkxc wrzhybdplumeqvaftbiosxgkxc wrzgyfdplumeqvaltbiosngcxc wrzhbfdzlumeqvajtbipsngkxc wrzhyfdplumwqeajtbiosngxxc wrzhyddplumeqvajtbimsngkxo wrzhyfdplumsqdajtfiosngkxc wrzvyfdplumeqjajtbkosngkxc wrzhyfddlumeqvujtbiosngxxc nrzhywdplumeqvajtbtosngkxc wzzhyfdplomeqvdjtbiosngkxc urzhyfdplumeqvaatbiosngkxb wtzyyfdplumeqvajtbiosngkxl wrzhyfdhlumnqvajebiosngkxc wrzhyfdplumpqvajtbiosnyjxc wrzdyfdplumeqvajtviospgkxc wrzhyfdplzmeqvajtbiosegfxc wrzhyfdglumkqvajtwiosngkxc wrzhtfdplgmeqvaqtbiosngkxc wrzhyfdplumeqvajxpiosngexc frzhyfdplvmeqvajtbiosngkxp wrzhyfkplumeqvajtbfosngkpc irzhyfqplumeqvajtbiosngktc wrzhyfdpluvoqvajtbioingkxc wrzhyfdplumemvautbuosngkxc wfzhyflplumeqvijtbiosngkxc wrzhyfdglumeqvzjkbiosngkxc wrzcyfdplaneqvajtbiosngkxc wrzhbfdplumeqwajtbiosngpxc szdhyfdplumeqvajtbiosngkxc krzhyfdplumeqvajtxilsngkxc brzhyfmplumexvajtbiosngkxc wrzhyvdplumeqvajtbiosiglxc orzhyfdplumeqvaotbcosngkxc hrzhyfdplumeqvaptbiocngkxc wwzhyfdklummqvajtbiosngkxc wrzhyfdppymeqvvjtbiosngkxc wsehyudplumeqvajtbiosngkxc wrzhyzpplumeqvajtbioscgkxc wrzhyfqpyumeqvajtbiusngkxc urzhyfdpdumeqmajtbiosngkxc wrzhyfdrlumxqvajtbiosnakxc wrthyfdplumeskajtbiosngkxc wrzhyfdplymeqvajtbiesbgkxc wrzhyfdqlumeqyajtciosngkxc trzhsfdplumeqyajtbiosngkxc wpzhyfdplimeqvajtbiosngfxc wrzhhfdplrmeqvajtbiosngkxx wrzhycdpgumeqvajtbioslgkxc wrzhyfdelumeqvajgtiosngkxc wrzhyfdplutebvajtbiosygkxc wrzhyfdplukeqvajtbiorngkec erzhyfdilumeqvajwbiosngkxc wrzhyfdplumnivxjtbiosngkxc wrzhyfdplumebvajtoiovngkxc wrzhyfdplumeqvajtbtosnwkxe wrzhyfxplumevvajtciosngkxc wbzhyfdxlumeqvajtriosngkxc wrzyyfdplumeqvajmbiosngkxb wfthyfdplumeqvajtbiosngkdc wrzhuedplumeqvajtbgosngkxc wrzhifdplumeqvajtsrosngkxc wrzhyfdplumqqvajtaiosngkac krzhyfvvlumeqvajtbiosngkxc wrzhyfdplzmeqqajtbiosngkvc wrzhyfqplumeqvajtbiosngdxy wrzhyfdpluieqvajtbiosngjjc wrzhyfdvlumeqrajdbiosngkxc wrzhcfdpxumeqvajtbiotngkxc wrzhyfdprumeqvaitbiosngexc wrzhygdplumeqvpjtbiasngkxc wrzhyndplumeqvajtpiosngkoc wrzhyfdplumedvajtriowngkxc wezjyndplumeqvajtbiosngkxc wrmhyfdplumewiajtbiosngkxc wrztyfdplumeqnajtbiobngkxc wrzhyfdpyumeqvajjbijsngkxc wrfhyfdplumeqkajtbiosnqkxc wrzhyfdpllmezvartbiosngkxc wszhyfdplumeqeajtbiqsngkxc wrzhyfdplumeqwajtbnosnikxc wrzvyfqulumeqvajtbiosngkxc drzhyfdplureqvajtbiqsngkxc wrzhyfdplumeqjamtbigsngkxc wrzhyfdplumeqvajbbiosngzrc grzhyfdpuumeqvajtbicsngkxc wrrhyfdplumeqvajtgiosnggxc wrzhyfkplumvqvajtbiosnhkxc wrzhyfdplumeqvajtbicspgkxa wrzhyfdplumeqvajtiiosnggoc wfzhyfdplumyqvaytbiosngkxc wrzhyfdpcumeqvajtbibsnfkxc wrzhyfdplumeqvajtbigsnzkmc wrzhyfdplcmeqvqjtriosngkxc wrzhypdplwmeqvajtbiosnvkxc wrziyfdmlumeqvaatbiosngkxc wkzhyfdplsmeqvajobiosngkxc wrzhyfdplumeqvkjvliosngkxc wzzbyfdplumeqvajtbiolngkxc wrzhyfdplvweqvajtbipsngkxc wrzhyvdplumeqvujtbiosnfkxc wrbhyfdplumedvajtbiosnhkxc wrzhofdplumeqvajtbiosnskxy wrzhyfdplumeqvaetbiohwgkxc wezhyfoplumeqvajtbiosngmxc wrzhykdblumeqvajtbiosngkjc wrzhyddplwmeqvajtbioungkxc wrzhyldplumqqvajpbiosngkxc wrzhyfdtlumeqvajtbiusngkcc wrzhyfdilumeqvajtbiosdgkxk wrvhywdplumeqvajtbiosngoxc wrzhyfdpyumeqvajtkiomngkxc wrzhyfdpbummqvajtbiosngcxc wrzhyfdpljmeqvajlbiosngjxc wrzhyfdpmuieqdajtbiosngkxc wrzgrfdplumeqvaktbiosngkxc wrzhgrdpluueqvajtbiosngkxc wazhyfdplhmeqvaqtbiosngkxc wruhyfdplumeqvujtliosngkxc wrzvyfdslumeqvajtbiwsngkxc wrzhyfdplumeivactbiosqgkxc wrzpyfdpbumeqvajtbioszgkxc bgzhyfdplumeyvajtbiosngkxc wrzhyfkplumeqvajtbiotngkxh wrdhyfhplumexvajtbiosngkxc brzhyfdpgumeqvgjtbiosngkxc arzhqfdpeumeqvajtbiosngkxc wrzhyftplumeqvajfbiosnykxc wrzhyfdpcumeqvartbtosngkxc wrzhvfzplumdqvajtbiosngkxc wrzhyfdjlumeqvaetbiosjgkxc wrbhyfdplumeqvajjjiosngkxc wrxhyfdplumeqvajtbiyskgkxc wpzhyydplumeqvajtbiosqgkxc wrzhyfdplumzqvajtbzoongkxc crzhyfdplnmeqvajtbjosngkxc wrzhyfdpluveusajtbiosngkxc wrzhypdpluyecvajtbiosngkxc wrnhyfdplumeqvajtbioengoxc wrzhypdplumefvajybiosngkxc wrzhyfdplumeqvattviosngkdc wrzhyfdplqmeqbajtbiostgkxc wrghyfdpluveqvajtbiosngkxn wryhyfdplumeqvajrbiopngkxc wrzhyqdplumeqvajtbiwsngkxt wrmoyfdpluheqvajtbiosngkxc wrghyfdnlumeqvyjtbiosngkxc wrzpyfdplumevvaatbiosngkxc wrzhyfdplumhqvajtbiodngxxc wrzhyfdplumeqcajtbioyjgkxc wrzhyfdplumeqvajabirsngkgc wrzhtfnplukeqvajtbiosngkxc wqzhyfdplumeqvajtbiopegkxc lrzhzfdplumeqdajtbiosngkxc wrzzyfdplumeqvajnkiosngkxc wrzhyfdflmmeqvajmbiosngkxc wrzoyfdplumeqvjjtpiosngkxc wrzhyfdpmpmeqvajhbiosngkxc krvhyfdplumeqvajtbiossgkxc wrzhyfdplumeqvaetviosnmkxc wrzhyfzclumeqvajtbiosngwxc wrzhyfdpvumeqvajtbiosngkcv wrzhyfdpoumeqvajtbiozngoxc wrzhyfwglumeqvajtbxosngkxc wrshyfdplumeqiajtbiosngklc wrzhyfdptdmeqvajtziosngkxc wrwhyfdplumeqqajtbiosngkxj wrzlyfypldmeqvajtbiosngkxc wrzhyfdplumeqvajtbirknikxc wrzhyhdplumeqvajtbmosnbkxc wyzhyfiplumeqvwjtbiosngkxc wrzgyfsplumeqvajtziosngkxc wrzhrfdllumepvajtbiosngkxc wrzayfdplumeqvajtbiosqgktc whzhyfdplemnqvajtbiosngkxc wazhyfdplumeqvaptbioongkxc wrzhyfdpluueqvajtbiosnglvc wrzhyfdplumjqvajmbionngkxc wrzhofdplvmeqvajtbiosnqkxc wrzhfidpluzeqvajtbiosngkxc wrzhlfdpcumeqvaatbiosngkxc wrzhyfdplumewvajtbiosigkoc wrzjyfdplwmeqvajtbiodngkxc wrzhyfdplumeqnsjtniosngkxc wrzxyfdplxmeqvajtbiosngkdc wrzhyfdplumpqvartbuosngkxc orzhyfdplumeqzsjtbiosngkxc wrzhyfdplumebvawtbiosngkxt wrztyfdulumeqvajtbiosngkxy wrzhytdplumeqvajtbznsngkxc wrzhvfdplumeqvajtbinsngkxu wezmyfdplumeqvajhbiosngkxc wrzhhftplumeqvajtaiosngkxc wrzhyfgplumeqvajtbioskgjxc ujuhyfdplumeqvajtbiosngkxc wryhymdplumeqvaftbiosngkxc wrzhygdplumeqvajibiosfgkxc frzhyfdppumeqvavtbiosngkxc wruhyfdylumeqvajzbiosngkxc wrzhzfdplumeqvajtbwosygkxc wrzhyfdplumeqvaeteiojngkxc wrhhyfdplumeqvajtiiopngkxc irzhyfdplumeqvajtbiosngryc wrvhycdpzumeqvajtbiosngkxc wrzhyfdplumyqwajtboosngkxc wrzthfdplumeqvajtbioengkxc mrzhyfdalumeqvajtbiosngcxc wrzhyfdflumpqvajtbiosngkmc wrzhyfxplumeqvajtbiosnvkxo wrzhyfdglumeqvajtbiosnfkxu wrzhyfdnrumeqpajtbiosngkxc wrzhlipplumeqvajtbiosngkxc wrzhkfdplumemvajtbiohngkxc wrziyfdmlumeqvaatbiosngkoc wrzhyfdplwceqvajtbiosngzxc wrzhyfdplumpqvactbiosngixc wdzhyfdhdumeqvajtbiosngkxc wnzhyfdplumeqvajtbbosngrxc wrzhyfdblumeqvajtbiosngvxw wyzhyfxpkumeqvajtbiosngkxc wrzhywdplumjqvajgbiosngkxc wrzhyfdpxumhqvajtbiokngkxc wrzhxfpplumeqvajtbiosngkxk mrzhyfdplumeqkajubiosngkxc drzhyfdplumeqvajtbioingnxc wrekyfdplumeqvajtbidsngkxc wrzhyfdplumeanyjtbiosngkxc wrzhyddpluzeqvajtbiosngtxc wrzfyfdplumeqvaqtbiosngkxr wrghywdpluneqvajtbiosngkxc wrzhyfdplueeqvaptbioyngkxc wrzhyqdpllmeqvajtbiosngdxc whzhyfdxlumeqvajtbiosngksc wrzjyfdplumeqvuitbiosngkxc brzhyfdplumeqhajtbiolngkxc wrzhyfqclureqvajtbiosngkxc wwzhpfdplureqvajtbiosngkxc wrzhyfdplumeqvavebijsngkxc wrzhyfdpuumeqvajtsiosnkkxc wrxhyfuplumeqvajtbiosngpxc wrzhyfdplumeqvujlbiospgkxc wrzvyfdplumeqvajtbiwsngpxc wrzhyndplumeqvajtbiwsnekxc wrzhkfdpoumeqvautbiosngkxc";
+    std::string segment;
+
+    std::stringstream input{data};
+    int c2{0}, c3{0};
+    while (input >> segment) {
+        std::map<char, uint8_t> chars;
+        for (auto c : segment) {
+            chars[c]++;
+        }
+
+        bool hasTwo{false};
+        bool hasThree{false};
+        for (auto count : chars) {
+            if (count.second == 2) {
+                hasTwo = true;
+            }
+            if (count.second == 3) {
+                hasThree = true;
+            }
+        }
+        if (hasTwo) {
+            c2++;
+        }
+        if (hasThree) {
+            c3++;
+        }
+    }
+
+    std::cout << "Solution part 1: " << (c2 * c3) << std::endl;
+
+    std::stringstream input2{data};
+    std::string strings[250];
+    std::string inputLine;
+    int var{0}, var2{0};
+
+    while (input2 >> inputLine) {
+        strings[var++] = inputLine;
+    }
+
+    for (var = 0; var < 250; var++) {
+        for (var2 = var + 1; var2 < 250; var2++) {
+            std::string string1 = strings[var];
+            const char *string2 = strings[var2].c_str();
+            char val1, val2;
+            uint8_t amountOfDifferences{0};
+
+            std::stringstream output;
+
+            for (auto c : string1) {
+                if (c != *string2++) {
+                    amountOfDifferences++;
+                } else {
+                    output << c;
+                }
+            }
+
+            if (amountOfDifferences == 1) {
+                std::cout << "Solution part 2: " << output.str();
+            }
+        }
+    }
+}
